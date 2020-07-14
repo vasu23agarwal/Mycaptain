ae7f4d5efb4814286fb5acb972a56cc239b5fb1f
vasu23agarwal <41936046+vasu23agarwal@users.noreply.github.com>
2020-07-14 16:29:03 +0530

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+list = [] 
+list.append(90)
+list.append('Vasu')
+list.insert(0,12)
+list
+
+
+# In[8]:
+
+
+list[1]
+
+
+# In[12]:
+
+
+tuple=(10,'abc',34)
+tuple
+
+
+# In[11]:
+
+
+tuple[1]
+
+
+# In[20]:
+
+
+detail={"name": "Hemkesh", "age": 23, "active": True}
+detail.pop('name')
+detail
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
